a614b7926a81bbcebb12fb41c27b4c840b9c082c
Gustavo I. López <lopezgus@gmail.com>
2023-04-19 19:15:28 -0300

    Anecdotes exercise 1.12 - v2

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -16,7 +16,10 @@ const Boton = ({text, conClick}) => {
 }
 
 const App = (props) => {
-  const click = () => (setSelected(random(anecdotes.length)));
+
+  const click = () => {
+    (setSelected(random(anecdotes.length-1)));
+  }
   const random = (max) => {
     return Math.floor((Math.random() * (max - 0 + 1)) + 0);
   }
@@ -34,12 +37,14 @@ const App = (props) => {
 }
 
 const anecdotes = [
-  'If it hurts, do it more often',
+  'If it hurts, do it more often.',
   'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.'
 ]
 
 ReactDOM.render(
